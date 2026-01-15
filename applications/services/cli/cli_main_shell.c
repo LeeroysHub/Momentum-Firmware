a40764597c27eb3f74673c3efbd9ee47ca90191d
Leeroy <135471162+LeeroysHub@users.noreply.github.com>
2026-01-15 17:53:09 +1100

    Make CLI Smaller, save RAM.

--- applications/services/cli/cli_main_shell.c
+++ applications/services/cli/cli_main_shell.c
@@ -7,27 +7,15 @@
 void cli_main_motd(void* context) {
     UNUSED(context);
     printf(ANSI_FLIPPER_BRAND_ORANGE
-           "\r\n"
-           "              _.-------.._                    -,\r\n"
-           "          .-\"```\"--..,,_/ /`-,               -,  \\ \r\n"
-           "       .:\"          /:/  /'\\  \\     ,_...,  `. |  |\r\n"
-           "      /       ,----/:/  /`\\ _\\~`_-\"`     _;\r\n"
-           "     '      / /`\"\"\"'\\ \\ \\.~`_-'      ,-\"'/ \r\n"
-           "    |      | |  0    | | .-'      ,/`  /\r\n"
-           "   |    ,..\\ \\     ,.-\"`       ,/`    /\r\n"
-           "  ;    :    `/`\"\"\\`           ,/--==,/-----,\r\n"
-           "  |    `-...|        -.___-Z:_______J...---;\r\n"
-           "  :         `                           _-'\r\n"
-           " _L_  _     ___  ___  ___  ___  ____--\"`___  _     ___\r\n"
+           " _ _  _     ___  ___  ___  ___  ____    ___  _     ___\r\n"
            "| __|| |   |_ _|| _ \\| _ \\| __|| _ \\   / __|| |   |_ _|\r\n"
            "| _| | |__  | | |  _/|  _/| _| |   /  | (__ | |__  | |\r\n"
            "|_|  |____||___||_|  |_|  |___||_|_\\   \\___||____||___|\r\n"
            "\r\n" ANSI_FG_BR_WHITE "Welcome to Flipper Zero Command Line Interface!\r\n"
-           "Read the manual: https://docs.flipper.net/development/cli\r\n"
-           "Run `help` or `?` to list available commands\r\n"
+           "Run `help` or `?` to list commands\r\n"
            "\r\n" ANSI_RESET);
 
-    const Version* firmware_version = furi_hal_version_get_firmware_version();
+    /* const Version* firmware_version = furi_hal_version_get_firmware_version();
     if(firmware_version) {
         printf(
             "Firmware version: %s %s (%s%s built on %s)\r\n",
@@ -36,7 +24,7 @@ void cli_main_motd(void* context) {
             version_get_githash(firmware_version),
             version_get_dirty_flag(firmware_version) ? "-dirty" : "",
             version_get_builddate(firmware_version));
-    }
+    }*/
 }
 
 const CliCommandExternalConfig cli_main_ext_config = {
