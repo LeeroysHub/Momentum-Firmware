3fa5e39e662c0ea856be85efca595043a51fe77b
Leeroy <135471162+LeeroysHub@users.noreply.github.com>
2026-01-31 16:16:35 +1100

    Add Spacer to RSSI Bar i SugGhz

--- applications/main/subghz/views/receiver.c
+++ applications/main/subghz/views/receiver.c
@@ -266,12 +266,16 @@ static void subghz_view_receiver_draw_frame(Canvas* canvas, uint16_t idx, bool s
 }
 
 static void subghz_view_rssi_draw(Canvas* canvas, SubGhzViewReceiverModel* model) {
+    //Add a 1px space between the segments
+    uint8_t spacer = 0;
     for(uint8_t i = 1; i < model->u_rssi; i++) {
         if(i % 5) {
-            canvas_draw_dot(canvas, 46 + i, 50);
-            canvas_draw_dot(canvas, 47 + i, 51);
-            canvas_draw_dot(canvas, 46 + i, 52);
-        }
+            uint8_t j = 46 + i + spacer;
+            canvas_draw_dot(canvas, j, 51);
+            canvas_draw_dot(canvas, j + 1, 52);
+            canvas_draw_dot(canvas, j, 53);
+        } else
+            spacer++;
     }
 }
 
